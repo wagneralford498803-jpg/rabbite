dd59a269419da404f1b15704ba3009dc55661e66
ttg <2117872716@qq.com>
2025-10-22 11:32:41 +0800

    layout静态结构搭建

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,15 @@
 </script>
 
 <template>
-
+<!-- 一级路由出口组件 -->
   <router-view></router-view>
+  <div class="footer">
+      地址：北京市海淀区
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped lang = scss>
+.footer{
+  color: $priceColor;
+}
+</style>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,6 +1,12 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
-  <div>
-    <h1>首页</h1>
-    <router-view></router-view>
-  </div>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
 </template>
